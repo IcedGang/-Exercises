074a172b9c477c98dd9cf5573b7a6c3d59b61c41
Vítor Coutinho <116609695+IcedGang@users.noreply.github.com>
2022-11-07 21:17:17 -0300

    Add files via upload

---
+++ Lista2/Ponto2DExemplo.java
@@ -0,0 +1,11 @@
+package Lista2;
+
+public class Ponto2DExemplo {
+    public static void main(String[] args) {
+        Ponto2D ponto = new Ponto2D(-4, 5);
+
+        ponto.imprimePonto();
+        System.out.println("Quadrante: " + ponto.quadrante());
+        System.out.printf("Distância: %.3f\n", ponto.distancia());
+    }
+}
